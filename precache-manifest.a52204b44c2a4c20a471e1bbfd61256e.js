46d0815dc75fe542459039e806be7a50b645c906
MikeG212 <michaelcgee93@gmail.com>
2019-08-09 16:01:21 -0700

    Updates

---
+++ precache-manifest.a52204b44c2a4c20a471e1bbfd61256e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686e53899a87e10530727a7667a505cc",
+    "url": "/MyTube/index.html"
+  },
+  {
+    "revision": "ec8dd955ca619e12db2d",
+    "url": "/MyTube/static/js/2.abde703d.chunk.js"
+  },
+  {
+    "revision": "93d5c437438b9a392fcd",
+    "url": "/MyTube/static/js/main.41a6d804.chunk.js"
+  },
+  {
+    "revision": "52ca25e7f81b0b381b36",
+    "url": "/MyTube/static/js/runtime~main.663f31e1.js"
+  }
+]);
